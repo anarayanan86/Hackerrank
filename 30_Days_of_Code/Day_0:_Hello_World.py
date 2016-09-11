44f3974111f087991dd1c5f23a5345f0612aaa60
Arjun Narayanan <anarayanan86@gmail.com>
2016-09-11 09:23:10 -0700

    Create Day_0:_Hello_World.py

---
+++ 30_Days_of_Code/Day_0:_Hello_World.py
@@ -0,0 +1,24 @@
+# Objective 
+# In this challenge, we review some basic concepts that will get you started with this series. You will need to use the same (or similar)
+# syntax to read input and write output in challenges throughout HackerRank. Check out the Tutorial tab for learning materials and an
+# instructional video!
+
+# Task 
+# To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally
+# print the value of your variable on a second line.
+
+# You've got this!
+
+# Note: The instructions are Java-based, but we support submissions in many popular languages. You can switch languages using the
+# drop-down menu above your editor, and the inputString variable may be written differently depending on the best-practice conventions
+# of your submission language.
+
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+inputString = raw_input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print "Hello, World."
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print inputString

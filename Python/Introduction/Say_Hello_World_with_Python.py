35351d6ea9d5912d62dbe43d315061841c3ddf7d
Arjun Narayanan <anarayanan86@gmail.com>
2016-11-09 08:59:33 -0800

    Create Say_Hello_World_with_Python.py

---
+++ Python/Introduction/Say_Hello_World_with_Python.py
@@ -0,0 +1,19 @@
+# Here is a sample line of code that can be executed in Python:
+# print("Hello, World!")
+# You can just as easily store a string as a variable and then print it to stdout:
+my_string = "Hello, World!"
+# print(my_string)
+# The above code will print Hello, World! on your screen. Try it yourself in the editor below!
+
+# Input Format
+# You do not need to read any input in this challenge.
+
+#Output Format
+# Print Hello, World! to stdout.
+
+# Sample Output
+
+# Hello, World!
+
+# Write your code on the next line. 
+print "Hello, World!"

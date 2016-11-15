f26e1c2518b426b8f241c141b6316ed7b159a175
Arjun Narayanan <anarayanan86@gmail.com>
2016-11-15 09:02:08 -0800

    Create Reading_Raw_Input.py

---
+++ Python/Introduction/Reading_Raw_Input.py
@@ -0,0 +1,17 @@
+# Reading Raw Input
+
+# Task 
+# Read a line of input from stdin and save it to a variable, s. Then print the contents of s to stdout.
+
+# Input Format
+# A single line containing sentence s.
+
+# Constraints
+# 1 <= s <= 500
+
+# Output Format
+# Print the contents of s to stdout.
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+s = raw_input()
+print s

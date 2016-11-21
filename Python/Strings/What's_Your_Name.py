7d1b0043e4369f156c59c5f96f33caa062d5214c
Arjun Narayanan <anarayanan86@gmail.com>
2016-11-21 09:49:08 -0800

    Create What's_Your_Name.py

---
+++ Python/Strings/What's_Your_Name.py
@@ -0,0 +1,19 @@
+# What's Your Name?
+
+# You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+# Hello firstname lastname! You just delved into python.
+
+# Input Format
+# The first line contains the first name, and the second line contains the last name.
+
+# Constraints
+# The length of the first and last name ≤ 10.
+
+# Output Format
+# Print the output as mentioned above.
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+firstname = raw_input()
+lastname = raw_input()
+
+print "Hello", firstname, lastname + "! You just delved into python."

89890be3df41eab64f5d4e9af24a093937b4d277
Arjun Narayanan <anarayanan86@gmail.com>
2016-11-28 08:58:28 -0800

    Create Ruby_Tutorial_-_Hello_World!.rb

---
+++ Ruby/Introduction/Ruby_Tutorial_-_Hello_World!.rb
@@ -0,0 +1,10 @@
+# Ruby Tutorial - Hello World!
+
+# Let's get started with Ruby!
+
+# Go ahead and type the following code in the code-editor:
+# print "Hello HackerRank!!"
+    
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+print "Hello HackerRank!!"

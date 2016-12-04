098d22bbc732a91f989dbc7054c9f7f217d8c37f
Arjun Narayanan <anarayanan86@gmail.com>
2016-12-04 08:32:26 -0800

    Update Ruby_Control_Structures_-_Each.rb

--- Ruby/Control_Structures/Ruby_Control_Structures_-_Each.rb
+++ Ruby/Control_Structures/Ruby_Control_Structures_-_Each.rb
@@ -2,7 +2,8 @@
 
 # Ruby offers control structures that let you iterate through its collections. One such control structure is each.
 # As you already know, HackerRank conducts many contests, and for every user who participates in a contest we update their score once the
-# contest ends. You will be given a method called scoring with an array passed as an argument. Elements of the array are of the class User.
+# contest ends. You will be given a method called scoring with an array passed as an argument. Elements of the array are of the class
+# User.
 # User class has a method update_score.
 # Your task is to iterate through each of the elements in the array using each and call the method update_score on every element.
 

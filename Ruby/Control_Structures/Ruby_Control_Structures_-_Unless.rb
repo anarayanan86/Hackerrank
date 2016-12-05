3b8dda11e6ce9e3806cb1f322ae4ff6c69e2e7e3
Arjun Narayanan <anarayanan86@gmail.com>
2016-12-05 09:18:31 -0800

    Update Ruby_Control_Structures_-_Unless.rb

--- Ruby/Control_Structures/Ruby_Control_Structures_-_Unless.rb
+++ Ruby/Control_Structures/Ruby_Control_Structures_-_Unless.rb
@@ -1,7 +1,7 @@
 # Ruby Control Structures - Unless
 
-# You've updated the score of every HackerRank user who participated in a contest. Sometimes, HackerRank admins also participate in a given
-# contest but care is taken to ensure that their submissions do not get any score and their score is not updated.
+# You've updated the score of every HackerRank user who participated in a contest. Sometimes, HackerRank admins also participate in a
+# given contest but care is taken to ensure that their submissions do not get any score and their score is not updated.
 # Like the previous challenge, you are given a method scoring with an array passed as an argument. Each element of the array is of class
 # User.
 # User has two public methods, is_admin? and update_score. Your task in this challenge is to use the control structure unless and update

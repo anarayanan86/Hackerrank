a3673f3b37be218c868608f71bddd9e1e7538d4d
Arjun Narayanan <anarayanan86@gmail.com>
2016-12-06 09:44:47 -0800

    Create Ruby_Control_Structures_-_Until.rb

---
+++ Ruby/Control_Structures/Ruby_Control_Structures_-_Until.rb
@@ -0,0 +1,15 @@
+# Ruby Control Structures - Until
+
+# This challenge has a beautiful one-liner answer.
+
+# "A hacker practices on HackerRank until getting to a rating of O(1) read as (Oh-one)"
+
+# Call the method coder.practice until coder.oh_one? becomes true.
+
+# Use the until control structure.
+
+# until is the logical equivalent of while not.
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+coder.practice until coder.oh_one? == true

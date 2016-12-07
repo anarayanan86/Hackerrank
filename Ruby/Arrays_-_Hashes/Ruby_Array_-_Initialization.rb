b284727006195067aa36dd16396a5f8fee3d23b2
Arjun Narayanan <anarayanan86@gmail.com>
2016-12-07 08:58:46 -0800

    Create Ruby_Array_-_Initialization.rb

---
+++ Ruby/Arrays_-_Hashes/Ruby_Array_-_Initialization.rb
@@ -0,0 +1,15 @@
+# Ruby Array - Initialization
+
+# One of the most commonly used data structures in Ruby is a Ruby Array, and below we see various methods of initializing a ruby array.
+
+# Your task is to initialize three different variables as explained below.
+
+# Initialize an empty array with the variable name array
+# Initialize an array with exactly one nil element in it with the variable name array_1
+# Initialize an array with exactly two elements with value 10 in it using the variable name array_2.
+
+
+# Initialize 3 variables here as explained in the problem statement
+array = []
+array_1 = [nil]
+array_2 = [10, 10]

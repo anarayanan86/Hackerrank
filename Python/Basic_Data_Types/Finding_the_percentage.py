29b03eebd4518f93ab85814547e18ed590b8baca
Arjun Narayanan <anarayanan86@gmail.com>
2016-12-08 08:07:55 -0800

    Update Finding_the_percentage.py

--- Python/Basic_Data_Types/Finding_the_percentage.py
+++ Python/Basic_Data_Types/Finding_the_percentage.py
@@ -1,9 +1,9 @@
 # Finding the percentage
 
 # You have a record of N students. Each record contains the student's name, and their percent marks in Maths, Physics and Chemistry. The
-# marks can be floating values. The user enters some integer N followed by the names and marks for N students. You are required to save the
-# record in a dictionary data type. The user then enters a student's name. Output the average percentage marks obtained by that student,
-# correct to two decimal places.
+# marks can be floating values. The user enters some integer N followed by the names and marks for N students. You are required to save
+# the record in a dictionary data type. The user then enters a student's name. Output the average percentage marks obtained by that
+# student, correct to two decimal places.
 
 # Input Format
 # The first line contains the integer N, the number of students. The next N lines contains the name and marks obtained by that student

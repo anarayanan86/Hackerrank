cc45b3fbf9a8624d0b009f988ed846b854fb2fae
Arjun Narayanan <anarayanan86@gmail.com>
2016-12-11 07:57:05 -0800

    Update Transpose_and_Flatten.py

--- Python/Numpy/Transpose_and_Flatten.py
+++ Python/Numpy/Transpose_and_Flatten.py
@@ -8,18 +8,15 @@
 # The tool flatten creates a copy of the input array flattened to one dimension.
 
 # Task
+# You are given a N X M integer array matrix with space separated elements (N = rows and M = columns). 
+# Your task is to print the transpose and flatten results.
 
-You are given a N X M integer array matrix with space separated elements (N = rows and M = columns). 
-Your task is to print the transpose and flatten results.
+# Input Format
+# The first line contains the space separated values of N and M. 
+# The next N lines contains the space separated elements of M columns.
 
-Input Format
-
-The first line contains the space separated values of N and M. 
-The next N lines contains the space separated elements of M columns.
-
-Output Format
-
-First, print the transpose array and then print the flatten.
+# Output Format
+# First, print the transpose array and then print the flatten.
 
 
 import numpy

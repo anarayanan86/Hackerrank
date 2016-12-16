ebe4c2e6362bd54e0232b7796514a5acd82bc10c
Arjun Narayanan <anarayanan86@gmail.com>
2016-12-16 08:38:07 -0800

    Update List_Comprehensions.py

--- Python/Basic_Data_Types/List_Comprehensions.py
+++ Python/Basic_Data_Types/List_Comprehensions.py
@@ -1,8 +1,8 @@
 # List Comprehensions
 
 # Let's learn about list comprehensions! You are given three integers X, Y and Z representing the dimensions of a cuboid along with an
-integer N. You have to print a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i + j + k  is not equal
-to N. Here, 0 <= i <= X; 0 <= j <= Y; 0 <= k <= Z 
+# integer N. You have to print a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i + j + k  is not equal
+# to N. Here, 0 <= i <= X; 0 <= j <= Y; 0 <= k <= Z 
 
 # Input Format
 # Four integers X, Y, Z and N each on four separate lines, respectively.

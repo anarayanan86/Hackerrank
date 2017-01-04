87cbc4c06e637c116dd6672eebc395d77bb78280
Arjun Narayanan <anarayanan86@gmail.com>
2017-01-04 09:46:58 -0800

    Update No_Idea!.py

--- Python/Sets/No_Idea!.py
+++ Python/Sets/No_Idea!.py
@@ -2,7 +2,8 @@
 
 # There is an array of n integers. There are also 2 disjoint sets, A and B, each containing m integers. You like all the integers in set A
 # and dislike all the integers in set B. Your initial happiness is 0. For each i integer in the array, if i is in A, you add 1 to your
-# happiness. If i is in B, you add -1 to your happiness. Otherwise, your happiness does not change. Output your final happiness at the end.
+# happiness. If i is in B, you add -1 to your happiness. Otherwise, your happiness does not change. Output your final happiness at the
+# end.
 
 # Note: Since A and B are sets, they have no repeated elements. However, the array might contain duplicate elements.
 

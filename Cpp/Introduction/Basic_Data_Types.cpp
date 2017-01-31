0a70db6a22d9e227df6f4a437f1f717a153dbe8a
Arjun Narayanan <anarayanan86@gmail.com>
2017-01-30 18:14:38 -0800

    Update Basic_Data_Types.cpp

--- Cpp/Introduction/Basic_Data_Types.cpp
+++ Cpp/Introduction/Basic_Data_Types.cpp
@@ -36,7 +36,7 @@ Input consists of the following space-separated values: int, long, long long, ch
 
 Output Format
 Print each element on a new line in the same order it was received as input. Note that the floating point value should be correct up to 3
-decimal places and the double to 9 decimal places.
+decimal places and the double to 9 decimal places. */
 
 
 #include <iostream>

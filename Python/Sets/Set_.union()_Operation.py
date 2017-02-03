4b92cafa2386a663758f2d336b2625b0676ecfc4
Arjun Narayanan <anarayanan86@gmail.com>
2017-02-02 20:19:05 -0800

    Update Set_.union()_Operation.py

--- Python/Sets/Set_.union()_Operation.py
+++ Python/Sets/Set_.union()_Operation.py
@@ -6,8 +6,8 @@
 # Set is immutable to the .union() operation (or | operation).
 
 # Task
-# The students of District College have subscriptions to English and French newspapers. Some students have subscribed only to English, some
-# have subscribed to only French and some have subscribed to both newspapers.
+# The students of District College have subscriptions to English and French newspapers. Some students have subscribed only to English,
+# some have subscribed to only French and some have subscribed to both newspapers.
 # You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is subscribed to the
 # French newspaper. The same student could be in both sets. Your task is to find the total number of students who have subscribed to at
 # least one newspaper.

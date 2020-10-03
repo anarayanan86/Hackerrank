737e85581a29b1566edc9f6824d4050d1f443c6c
gajanan badge <gajubadge11@gmail.com>
2020-10-03 23:34:08 +0530

    adding some examples
    easily grasp through real time examples

--- Python/Sets/Introduction_to_Sets.py
+++ Python/Sets/Introduction_to_Sets.py
@@ -4,6 +4,27 @@
 # When printed, iterated or converted into a sequence, its elements will appear in an arbitrary order.
 
 # Basically, sets are used for membership testing and eliminating duplicate entries. 
+# let's get it through the example
+# >> print set()
+# set([])
+
+# >>> print set('HackerRank')
+# set(['a', 'c', 'e', 'H', 'k', 'n', 'r', 'R'])
+
+# >>> print set([1,2,1,2,3,4,5,6,0,9,12,22,3])
+# set([0, 1, 2, 3, 4, 5, 6, 9, 12, 22])
+
+# >>> print set((1,2,3,4,5,5))
+# set([1, 2, 3, 4, 5])
+
+# >>> print set(set(['H','a','c','k','e','r','r','a','n','k']))
+# set(['a', 'c', 'r', 'e', 'H', 'k', 'n'])
+
+# >>> print set({'Hacker' : 'DOSHI', 'Rank' : 616 })
+# set(['Hacker', 'Rank'])
+
+# >>> print set(enumerate(['H','a','c','k','e','r','r','a','n','k']))
+# set([(6, 'r'), (7, 'a'), (3, 'k'), (4, 'e'), (5, 'r'), (9, 'k'), (2, 'c'), (0, 'H'), (1, 'a'), (8, 'n')])
 
 # Task
 # Now, let's use our knowledge of sets and help Mickey.

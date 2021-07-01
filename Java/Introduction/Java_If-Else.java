dce9b8cb46c30d0e2f21f7f2a5415440e09ee34c
sachinsingh24 <77788607+sachinsingh24@users.noreply.github.com>
2021-07-02 01:49:04 +0530

    Update Java_If-Else.java

--- Java/Introduction/Java_If-Else.java
+++ Java/Introduction/Java_If-Else.java
@@ -31,25 +31,15 @@ import java.io.*;
 
         public static void main(String[] args) {
 
-            Scanner sc=new Scanner(System.in);
-            int n=sc.nextInt();            
-            String ans="";
-            if(n%2==1){
-              ans = "Weird";
-            }
-            else {
-                if (n >= 2 && n <= 5) {
-                    ans = "Not Weird";
-                } else if (n >= 6 && n <= 20) {
-                    ans = "Weird";
-                } else {
-                    ans = "Not Weird";
-                }
-            
-               //Complete the code
-                
-            }
-            System.out.println(ans);
+            Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        if(n % 2 == 1 || (n >= 6 && n <=20)) {
+            System.out.println("Weird");
+        }
+        else {
+          System.out.println("Not Weird");
+        }
             
         }
     }
